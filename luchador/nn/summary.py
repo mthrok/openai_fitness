df1c0199056911b7f0c62d2901ebdb375038c16f
moto (・_・) <mthrok@gmail.com>
2016-10-20 17:17:06 -0700

    Refactor SummaryWroter and add convenient function

--- luchador/nn/summary.py
+++ luchador/nn/summary.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import logging
 
+import numpy as np
 import tensorflow as tf
 
 _LG = logging.getLogger(__name__)
@@ -11,48 +12,84 @@ __all__ = ['SummaryWriter']
 
 
 class SummaryOperation(object):
-    """Create placeholder and summary operations for the given tensors"""
-    def __init__(self, summary_type, names):
+    """Create placeholder and summary operations for the given names"""
+    def __init__(self, type, name):
         summary_funcs = {
             'scalar': tf.scalar_summary,
             'image': tf.image_summary,
             'audio': tf.audio_summary,
             'histogram': tf.histogram_summary,
         }
-        func = summary_funcs[summary_type]
-
-        self.ops, self.pfs = [], []
-        for name in names:
-            pf = tf.placeholder('float32')
-            op = func(name, pf)
-            self.ops.append(op)
-            self.pfs.append(pf)
+        self.pf = tf.placeholder('float32')
+        self.op = summary_funcs[type](name, self.pf)
 
 
 class SummaryWriter(object):
     def __init__(self, output_dir, graph=None):
         self.output_dir = output_dir
-
         self.summary_ops = {}
-        self.placeholders = []
+
         self.writer = tf.train.SummaryWriter(self.output_dir)
         self.graph = tf.Graph()
         self.session = tf.Session(graph=self.graph)
 
+    ###########################################################################
+    # Basic functionalitites
     def add_graph(self, graph=None, global_step=None):
-        if graph:
-            self.writer.add_graph(graph, global_step=global_step)
+        self.writer.add_graph(graph, global_step=global_step)
 
-    def register(self, key, summary_type, names):
+    def register(self, type, names, tag=None):
         with self.graph.as_default():
             with self.graph.device('/cpu:0'):
-                self.summary_ops[key] = SummaryOperation(summary_type, names)
+                self._register(type, names)
 
-    def summarize(self, key, global_step, values):
-        cfg = self.summary_ops[key]
-        feed_dict = {pf: value for pf, value in zip(cfg.pfs, values)}
+    def _register(self, type, names, tag=None):
+        for name in names:
+            self.summary_ops[name] = SummaryOperation(type, name)
 
-        summaries = self.session.run(cfg.ops, feed_dict=feed_dict)
+    def summarize(self, global_step, dataset):
+        """Summarize the dataset
+
+        Args:
+          global_step (int): Global step
+
+          dataset (dict):
+            Keys correspond to registered summary operation and
+            values correspond to the actual values to summaryze
+        """
+        ops, feed_dict = [], {}
+        for name, value in dataset.items():
+            ops.append(self.summary_ops[name].op)
+            feed_dict[self.summary_ops[name].pf] = value
+
+        summaries = self.session.run(ops, feed_dict=feed_dict)
         for summary in summaries:
             self.writer.add_summary(summary, global_step)
         self.writer.flush()
+
+    ###########################################################################
+    # Convenient functions
+    def register_stats(self, names):
+        """For each name, create 'name/[Average, Min, Max]' summary ops"""
+        all_names = ['{}/{}'.format(name, stats) for name in names
+                     for stats in ['Average', 'Min', 'Max']]
+        self.register('scalar', all_names)
+
+    def summarize_stats(self, global_step, dataset):
+        """Summarize statistics of dataset
+
+        Args:
+          global_step (int): Global step
+
+          dataset (dict):
+            - Key (str): Names used in `register_stats`
+            - Value (list of floats, or NumPy Array): Values to summarize stats
+        """
+        for name, values in dataset.items():
+            _dataset = {
+                '{}/Average'.format(name): np.mean(values),
+                '{}/Min'.format(name): np.min(values),
+                '{}/Max'.format(name): np.max(values)
+            }
+            self.summarize(global_step, _dataset)
+    ###########################################################################
