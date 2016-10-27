c0bb8ad47464c5cfa15ebd636edbd6c376ea48f4
moto (・_・) <mthrok@gmail.com>
2016-10-26 20:17:40 -0700

    Tweak layer test

--- tests/integration/run_layer_numerical_compatibility_tests.sh
+++ tests/integration/run_layer_numerical_compatibility_tests.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-set -eu
+set -u
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATA_DIR="${BASE_DIR}/data/layer"
 TEST_DIR="${BASE_DIR}/test_layer_numerical_compatibility"
 TEST_COMMAND="${TEST_DIR}/test_layer_numerical_compatibility.sh"
 
+RETURN=0
 for FILE in ${DATA_DIR}/*.yml
 do
-    "${TEST_COMMAND}" --config "${FILE}"
+    "${TEST_COMMAND}" "${FILE}"
+    if [[ ! $? = 0 ]]; then
+	RETURN=1
+    fi
 done
+
+exit ${RETURN}
