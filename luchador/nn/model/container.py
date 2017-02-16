8e3e2a7f6db5bb03c0108cf2151100dfd5338736
moto (・_・) <mthrok@gmail.com>
2017-02-16 15:28:24 -0800

    Remove name parameter

--- luchador/nn/model/container.py
+++ luchador/nn/model/container.py
@@ -9,8 +9,8 @@ from .base_model import BaseModel
 class Container(BaseModel):
     """Data structure for handling multiple network architectures at once
 
-    As a result, using this class and build utility functions,
-    multi-branching-merging network is built easily.
+    Using this class and build utility functions make it easy to build
+    multi-branching-merging network.
     """
     def __init__(self):
         super(Container, self).__init__()
@@ -32,67 +32,40 @@ class Container(BaseModel):
         self.models[name] = model
         return self
 
-    def get_parameter_variables(self, name=None):
+    def get_parameter_variables(self):
         """Get Variable objects consisting the parameters of this model
-
-        Parameters
-        ----------
-        name : str or None
-            Name of model from which parameter variables are retrieved.
-            If None, parameters from all the models are fetched.
 
         Returns
         -------
         list
             List of Variables from layer parameters
         """
-        if name:
-            return self.models[name].get_parameter_variables()
-
         ret = []
         for name_ in self.models.keys():
             ret.extend(self.models[name_].get_parameter_variables())
         return ret
 
-    def get_output_tensors(self, name=None):
+    def get_output_tensors(self):
         """Get Tensor s which represent the output of each layer of this model
-
-        Parameters
-        ----------
-        name : str or None
-            Name of model from which parameter variables are retrieved.
-            If None, parameters from all the models are fetched.
 
         Returns
         -------
         list
             List of Tensors each of which hold output from layer
         """
-        if name:
-            return self.models[name].get_output_tensors()
-
         ret = []
         for name_ in self.models.keys():
             ret.extend(self.models[name_].get_output_tensors())
         return ret
 
-    def get_update_operations(self, name=None):
+    def get_update_operations(self):
         """Get update opretaions from each layer of this model
-
-        Parameters
-        ----------
-        name : str or None
-            Name of model from which parameter variables are retrieved.
-            If None, parameters from all the models are fetched.
 
         Returns
         -------
         list
             List of update operations from each layer
         """
-        if name:
-            return self.models[name].get_update_operations()
-
         ret = []
         for name_ in self.models.keys():
             ret.extend(self.models[name_].get_update_operations())
