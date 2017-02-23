2c1d50ef44be70792ada6a04d6757e7625f1455f
moto (・_・) <mthrok@gmail.com>
2017-02-22 17:58:52 -0800

    Refactor base layer

---
+++ luchador/nn/base/layer/__init__.py
@@ -0,0 +1,11 @@
+"""Unstructure Layer classes for export"""
+from __future__ import absolute_import
+# pylint: disable=wildcard-import
+from .base import *  # noqa
+from .linear import *  # noqa
+from .math import *  # noqa
+from .reshape import *  # noqa
+from .activation import *  # noqa
+from .convolution import *  # noqa
+from .normalization import *  # noqa
+from .misc import *  # noqa
