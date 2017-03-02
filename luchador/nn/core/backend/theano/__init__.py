e0f4bd4659eaa41a098b790412a22b49c87389cc
moto (・_・) <mthrok.ca@gmail.com>
2017-03-02 14:25:54 -0800

    Clean up import

--- luchador/nn/core/backend/theano/__init__.py
+++ luchador/nn/core/backend/theano/__init__.py
@@ -1,18 +1,11 @@
 """Implement NN components in Theano backend"""
 from __future__ import absolute_import
-# pylint: disable=wildcard-import
-from . import (  # noqa: F401
-    initializer,
-    layer,
-    cost,
-    optimizer,
+from . import (  # noqa
+    wrapper,
     session,
+    initializer,
+    cost,
+    layer,
+    optimizer,
+    ops,
 )
-from .wrapper import (  # noqa: F401
-    Input,
-    Variable,
-    Tensor,
-    Operation,
-    get_variable,
-)
-from .misc import *  # noqa: F401, F403
--- luchador/nn/core/impl/__init__.py
+++ luchador/nn/core/impl/__init__.py
@@ -1,8 +1,11 @@
 """Initialize Neural Network module"""
 from __future__ import absolute_import
-# pylint: disable=import-self
-from . import initializer  # noqa
-from . import cost  # noqa
-from . import optimizer  # noqa
-from . import layer  # noqa
-from .session import Session  # noqa
+from . import (  # noqa
+    scope,
+    wrapper,
+    session,
+    initializer,
+    cost,
+    layer,
+    optimizer,
+)
