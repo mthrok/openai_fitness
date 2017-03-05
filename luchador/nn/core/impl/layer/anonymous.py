b97fde4f7abea1f2e5335ccb6b5d6d5e51c59981
moto (・_・) <mthrok.ca@gmail.com>
2017-03-05 15:29:00 -0800

    Fix

--- luchador/nn/core/impl/layer/anonymous.py
+++ luchador/nn/core/impl/layer/anonymous.py
@@ -10,13 +10,25 @@ __all__ = ['Anonymous']
 # pylint: disable=abstract-method
 
 
-def _get_safe_function(*args, **kwargs):
+def _parse_input_tensors(input_tensor, *args, **kwargs):
+    if input_tensor:
+        if kwargs:
+            raise ValueError(
+                'Anonymouse layer accepsts either positional parameters '
+                'or keyword arguments, not both.')
+        if args:
+            return [input_tensor] + args
+        return input_tensor
+    return kwargs
+
+
+def _get_safe_function(input_tensor, *args, **kwargs):
     if args and kwargs:
         raise ValueError(
             'Anonymouse layer accepsts either positional parameters '
             'or keyword arguments, not both.')
     maps = {
-        'x': args if args else kwargs,
+        'x': _parse_input_tensors(input_tensor, *args, **kwargs),
         'True': True,
         'False': False,
         'NormalRandom': random.NormalRandom,
@@ -32,12 +44,15 @@ class Anonymous(BaseLayer):
     def __init__(self, exp, name='Anonymous'):
         super(Anonymous, self).__init__(name=name, exp=exp)
 
-    def build(self, *args, **kwargs):
+    def build(self, input_tensor=None, *args, **kwargs):
         """Build Anonymous layer
 
 
         """
+        return self._build(self, input_tensor=None, *args, **kwargs)
+
+    def _build(self, input_tensor=None, *args, **kwargs):
         # pylint: disable=eval-used
-        local = _get_safe_function(*args, **kwargs)
+        local = _get_safe_function(input_tensor, *args, **kwargs)
         y = eval(self.args['exp'], {'__builtins__': None}, local)
         return wrapper.Tensor(tensor=y.unwrap(), shape=y.shape, name='output')
