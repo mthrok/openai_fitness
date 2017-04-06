26476d19890c847f60bc1a305f6e8a9e88568585
moto (・_・) <mthrok.ca@gmail.com>
2017-04-06 12:22:33 -0700

    Remove register and simplify

--- tests/unit/nn/summary_test.py
+++ tests/unit/nn/summary_test.py
@@ -32,6 +32,11 @@ def _gen_random_value():
 
 
 class SummaryWriterTest(TestCase):
+    """Test case for SummaryWriter.
+
+    Since it is difficult to check the event file produced by Tensorflow native
+    summary operations, this test only checks if they run without crush.
+    """
     def _get_empty_dir(self):
         output_dir = os.path.join(OUTPUT_DIR, self.id().split('.')[-1])
         _remove_files(output_dir)
@@ -43,40 +48,51 @@ class SummaryWriterTest(TestCase):
         writer = SummaryWriter(output_dir)
 
         key = 'test_scalar'
-        writer.register('scalar', names=[key])
         for i in range(10):
             value = i + np.random.rand()
-            writer.summarize(global_step=i, dataset={key: value})
+            writer.summarize(
+                summary_type='scalar', global_step=i, dataset={key: value})
 
     def test_histogram_summarize(self):
         """`summarize` function summarize histogram"""
         output_dir = self._get_empty_dir()
         writer = SummaryWriter(output_dir)
 
-        key = 'test_histogram'
-        writer.register('histogram', names=[key])
         for i in range(10):
-            value = i + i * np.random.rand(32, 8, 8, 4)
-            writer.summarize(global_step=i, dataset={key: value})
+            dataset = {'test_histogram': i + i * np.random.rand(32, 8, 8, 4)}
+            writer.summarize(
+                summary_type='histogram', global_step=i, dataset=dataset)
 
     def test_image_summary(self):
         """`summarize` function summarize image"""
         output_dir = self._get_empty_dir()
         writer = SummaryWriter(output_dir)
 
-        key = 'test_image'
-        writer.register('image', names=[key])
         for i in range(10):
-            value = i + i * np.random.rand(32, 8, 8, 4)
-            writer.summarize(global_step=i, dataset={key: value})
+            dataset = {'test_image': i + i * np.random.rand(32, 8, 8, 4)}
+            writer.summarize(
+                summary_type='image', global_step=i, dataset=dataset)
 
     def test_audio_summary(self):
         """`summarize` function summarize audio"""
         output_dir = self._get_empty_dir()
         writer = SummaryWriter(output_dir)
 
-        key = 'test_audio'
-        writer.register('audio', names=[key])
         for i in range(10):
-            value = np.random.randn(32, 1440)
-            writer.summarize(global_step=i, dataset={key: value})
+            dataset = {'test_audio': np.random.randn(32, 1440)}
+            writer.summarize(
+                summary_type='audio',
+                global_step=i, dataset=dataset, sample_rate=144)
+
+    def test_stats_summary(self):
+        """`summarize_stats` function summarize max/min/agerage"""
+        output_dir = self._get_empty_dir()
+        writer = SummaryWriter(output_dir)
+
+        for i in range(10):
+            dataset = {
+                'test_stats_1': np.random.randn(100) - i,
+                'test_stats_2': np.random.randn(100),
+                'test_stats_3': np.random.randn(100) + i,
+            }
+            writer.summarize_stats(global_step=i, dataset=dataset)
