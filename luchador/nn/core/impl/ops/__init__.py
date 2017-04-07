25a0e58de555ce5dd2187f73b4562469bd195254
moto (・_・) <mthrok.ca@gmail.com>
2017-04-07 09:27:52 -0700

    Fix

--- luchador/nn/core/impl/ops/__init__.py
+++ luchador/nn/core/impl/ops/__init__.py
@@ -1,6 +1,6 @@
 """Define operations"""
 from __future__ import absolute_import
-# pylint: disable=unused-import, redefined-builtin
+# pylint: disable=unused-import,redefined-builtin
 from .clip import clip_by_value, clip_by_norm  # noqa
 from .grad import compute_gradient  # noqa
 from .misc import build_sync_op, one_hot  # noqa
