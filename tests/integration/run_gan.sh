63fedcde3bbc0f173d3386723ca90e5bab0bf1c3
moto (・_・) <mthrok.ca@gmail.com>
2017-07-15 11:24:48 -0700

    Combine tests

--- tests/integration/run_gan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-DATA="${HOME}/.mnist/mnist.pkl.gz"
-if [ ! -f "${DATA}" ]; then
-    echo "Downloading MNIST"
-    curl --create-dirs -o "${DATA}" http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
-fi
-python example/gan/train_gan.py --mnist "${DATA}"
