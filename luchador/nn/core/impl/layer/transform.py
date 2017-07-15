0852c4410a909db36308daf2f1ad437ca505d173
moto (・_・) <mthrok.ca@gmail.com>
2017-07-15 16:38:52 -0700

    Add initial working impl

--- luchador/nn/core/impl/layer/transform.py
+++ luchador/nn/core/impl/layer/transform.py
@@ -2,11 +2,16 @@
 from __future__ import division
 from __future__ import absolute_import
 
+import logging
+
+from luchador.configure import get_nn_conv_format
 from ...base import BaseLayer
 from ...backend import layer
 from ...backend import ops
 __all__ = ['Flatten', 'Concat', 'Reshape']
 # pylint: disable=abstract-method
+
+_LG = logging.getLogger(__name__)
 
 
 class Flatten(layer.Flatten, BaseLayer):
@@ -21,11 +26,26 @@ class Concat(layer.Concat, BaseLayer):
         super(Concat, self).__init__(axis=axis, scope=scope)
 
 
+def _convert_to_running_format(shape, given_format):
+    running_format = get_nn_conv_format()
+    if given_format == running_format:
+        return shape
+    if running_format == 'NCHW':
+        _LG.info('  * Converting `shape` to NCHW')
+        return (shape[0], shape[3], shape[1], shape[2])
+    _LG.info('  * Converting `shape` to NHWC')
+    return (shape[0], shape[2], shape[3], shape[1])
+
+
 class Reshape(BaseLayer):
     """Reshape variable"""
-    def __init__(self, shape, scope='Reshape'):
-        super(Reshape, self).__init__(shape=shape, scope=scope)
+    def __init__(self, shape, scope='Reshape', shape_format=None):
+        super(Reshape, self).__init__(
+            shape=shape, scope=scope, shape_format=shape_format)
 
     def _build(self, input_tensor):
+        given_format = self.args['shape_format']
         shape, scope = self.args['shape'], self.args['scope']
+        if given_format:
+            shape = _convert_to_running_format(shape, given_format)
         return ops.reshape(input_tensor, shape, name=scope)
