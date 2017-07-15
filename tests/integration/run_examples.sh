61037566183a195a5f94be405b0e1e4594b74a50
moto (・_・) <mthrok.ca@gmail.com>
2017-07-15 11:44:59 -0700

    Fix output

--- tests/integration/run_examples.sh
+++ tests/integration/run_examples.sh
@@ -7,7 +7,7 @@ if [ ! -f "${DATA}" ]; then
     curl --create-dirs -o "${DATA}" http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
 fi
 
-python example/classification/classify_mnist.py --mnist "${DATA}" --output tmp/mnist  --model example/classification/model.yml
-python example/autoencoder/train_ae.py          --mnist "${DATA}" --output tmp/ae/ae  --model example/autoencoder/autoencoder.yml
-python example/autoencoder/train_ae.py          --mnist "${DATA}" --output tmp/ae/vae --model example/autoencoder/variational_autoencoder.yml
-python example/gan/train_gan.py                 --mnist "${DATA}" --output tmp/gan    --model example/gan/model.yml
+python example/classification/classify_mnist.py --mnist "${DATA}" --model example/classification/model.yml
+python example/autoencoder/train_ae.py          --mnist "${DATA}" --model example/autoencoder/autoencoder.yml
+python example/autoencoder/train_ae.py          --mnist "${DATA}" --model example/autoencoder/variational_autoencoder.yml
+python example/gan/train_gan.py                 --mnist "${DATA}" --model example/gan/model.yml
